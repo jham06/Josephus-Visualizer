425078cd7020ec2b275e083921fd7a01d7a1f5f2
Justin Ham <hham1@ualberta.ca>
2026-07-04 21:52:25 -0600

    changing some bullshit

--- src/numbers.jsx
+++ src/numbers.jsx
@@ -1,7 +1,7 @@
 
 function Numbers (props) {
 
-    // now using props, i should be able to import the functions into this file?
+    // now using props, i should be able to import the functions into this file? testing
     return (<div className="counter-container"> 
         <p className='count-display'> People: {props.people}</p>
         <button disabled={props.isRunning} className='counter-button' onClick={props.decrementN} >🔽</button>
