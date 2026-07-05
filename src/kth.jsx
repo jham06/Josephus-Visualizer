6c1ed2ed2d306feb4d77cf28069e817883c04e6b
Justin Ham <hham1@ualberta.ca>
2026-07-04 22:16:30 -0600

    emoji changes

--- src/kth.jsx
+++ src/kth.jsx
@@ -4,9 +4,9 @@ function Kth (props) {
     // now using props, i should be able to import the functions into this file?
     return (<div className="counter-container"> 
         <p className='count-display'> Steps: {props.steps}</p>
-        <button disabled={props.isRunning} className='counter-button' onClick={props.decrementK} >🔽</button>
-        <button disabled={props.isRunning} className='counter-button' onClick={props.resetK}>🔄RESET</button>
-        <button disabled={props.isRunning} className='counter-button' onClick={props.incrementK}>🔼</button>
+        <button disabled={props.isRunning} className='counter-button' onClick={props.decrementK} >▼</button>
+        <button disabled={props.isRunning} className='counter-button' onClick={props.resetK}>⟳ RESET</button>
+        <button disabled={props.isRunning} className='counter-button' onClick={props.incrementK}>▲</button>
         </div>
     )
 }
--- src/numbers.jsx
+++ src/numbers.jsx
@@ -4,9 +4,9 @@ function Numbers (props) {
     // now using props, i should be able to import the functions into this file? testing
     return (<div className="counter-container"> 
         <p className='count-display'> People: {props.people}</p>
-        <button disabled={props.isRunning} className='counter-button' onClick={props.decrementN} >🔽</button>
-        <button disabled={props.isRunning} className='counter-button' onClick={props.resetN}>🔄RESET</button>
-        <button disabled={props.isRunning} className='counter-button' onClick={props.incrementN}>🔼</button>
+        <button disabled={props.isRunning} className='counter-button' onClick={props.decrementN} >▼</button>
+        <button disabled={props.isRunning} className='counter-button' onClick={props.resetN}>⟳ RESET</button>
+        <button disabled={props.isRunning} className='counter-button' onClick={props.incrementN}>▲</button>
         </div>
     )
 }
